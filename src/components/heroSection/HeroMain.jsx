1e10bbe915c6b38f54ef007bf429be6a58a9425e
DilipBsr <dilipkr.bs1999@gmail.com>
2025-08-03 12:11:01 +0530

    UI Updated

--- src/components/heroSection/HeroMain.jsx
+++ src/components/heroSection/HeroMain.jsx
@@ -3,7 +3,7 @@ import HeroPic from "./HeroPic";
 
 const HeroMain = () => {
   return (
-    <div className=" pt-40 pb-16">
+    <div className=" pt-40 pb-16 ">
       <div className="flex lg:flex-row 
       md:flex-row flex-col  max-w-[1200px] mx-auto justify-between items-center relative px-4">
         <HeroText />
--- src/components/navbar/NavbarMain.jsx
+++ src/components/navbar/NavbarMain.jsx
@@ -6,7 +6,7 @@ const NavbarMain = () => {
   const menuOpen = useSelector((state) => state.menu.menuOpen);
   return (
     <nav className="max-w-[1300px] mx-auto w-full px-4 fixed left-[50%] -translate-x-[50%] z-20 flex gap-4 mt-2">
-      <div className="flex justify-between w-full max-w-[1200px] mx-auto  bg-black items-center pl-6 pr-6 lg:p-4 rounded-3xl shadow-2xl shadow-orange-200">
+      <div className="flex justify-between w-full max-w-[1200px] mx-auto  bg-black opacity-40 items-center pl-6 pr-6 lg:p-4 rounded-3xl shadow-2xl shadow-orange-200">
         <h1 className="text-white  text-3xl hidden font-special font-extrabold lg:block md:block">
           Portfolio
         </h1>
@@ -20,7 +20,7 @@ const NavbarMain = () => {
         Contact Me
       </button>
       </div>
-      <div className="flex lg:hidden sm:block p-4 bg-black items-center justify-center rounded-3xl  border-orange border-[0.5px] ">
+      <div className="flex lg:hidden sm:block p-4 bg-black opacity-40 items-center justify-center rounded-3xl  border-orange border-[0.5px] ">
         <NavbarToggler />
       </div>
     </nav>
