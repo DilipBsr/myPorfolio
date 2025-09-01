290253334e8bce666dd95f5290b78c2946021e5a
DilipBsr <dilipkr.bs1999@gmail.com>
2025-09-01 14:37:39 +0530

    Navbar

--- src/components/navbar/NavbarLinks.jsx
+++ src/components/navbar/NavbarLinks.jsx
@@ -1,16 +1,24 @@
+import { useDispatch } from "react-redux";
 import { Link } from "react-scroll";
+import { toggleMenu } from "../../state/menuSlice";
 
 const links = [
   { link: "About Me", section: "about" },
   { link: "Skills", section: "skills" },
-  { link: "Experience", section: "experience" },
+  { link: "Certificate", section: "certificate" },
   { link: "Projects", section: "projects" },
   { link: "Contact", section: "contact" },
 ];
 
 const NavbarLinks = () => {
+  const dispatch = useDispatch();
+
+  const setToggleMenu = () => {
+    dispatch(toggleMenu());
+    console.log("clicked");
+  };
   return (
-    <ul className="flex lg:flex-row flex-col gap-6 text-white font-body lg:relative absolute top-[120%] text-center left-[50%] -translate-x-[50%] lg:text-md text-xl bg-cyan/30 bg-black w-full py-4">
+    <ul className="flex lg:flex-row flex-col gap-6 text-white font-body lg:relative absolute top-[120%] text-center left-[50%] -translate-x-[50%] lg:text-md text-xl bg-black w-full py-4">
       {links.map((link, index) => {
         return (
           <li key={index} className="group">
@@ -21,6 +29,7 @@ const NavbarLinks = () => {
               offset={-130}
               to={link.section}
               className="cursor-pointer text-white hover:text-cyan transition-all duration-500"
+              onClick={setToggleMenu}
             >
               {link.link}
             </Link>
