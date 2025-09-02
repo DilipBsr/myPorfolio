1be95b81c51af4eb748ee1f9a701ff50903263ad
DilipBsr <dilipkr.bs1999@gmail.com>
2025-09-02 16:41:16 +0530

    About Section

--- src/components/aboutMeSection/AboutMeImage.jsx
+++ src/components/aboutMeSection/AboutMeImage.jsx
@@ -3,7 +3,7 @@ const AboutMeImage = () => {
     <div className="h-[300px] w-[300px] relative">
       <div className="h-[300px] w-[300px] rounded-[100px] absolute overflow-hidden">
         <img
-          src="../images/about-me.jpg"
+          src="../images/about-me.png"
           alt="About Me Image"
           className="h-full w-auto object-cover"
         />
--- src/components/subHeroSection/SubHeroMain.jsx
+++ src/components/subHeroSection/SubHeroMain.jsx
@@ -1,10 +1,31 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import { Autoplay } from "swiper/modules";
+import "swiper/css/autoplay";
+
 const SubHeroMain = () => {
   return (
-    <div className="w-full border-y bg-brown border-lightGrey text-lightGrey flex justify-around uppercase xl:text-4xl md:text-2xl sm:text-4xl py-8 md:flex-row sm:flex-col items-center gap-4">
-      <p className="md:block sm:hidden">Fast Learner</p>
-      Adaptable
-      <p className="md:block sm:hidden">Team Player</p>
+    <>
+    <div className="w-full border-y border-light-200 text-light-200 flex justify-around uppercase xl:text-3xl md:text-2xl sm:text-2xl  py-8 md:flex-row items-center gap-4 outfit-regular sm:flex hidden">
+        <p>Adaptable</p>
+        <p>Problem Solver</p>
+        <p >Team Player</p>
     </div>
+     <div className="w-full border-y border-gray-200 text-light-200 flex text-center uppercase py-8 outfit-regular sm:hidden">
+        <Swiper spaceBetween={20} slidesPerView={1}  loop={true}
+        speed={800}
+            autoplay={{
+            delay: 3000,
+            disableOnInteraction: false,
+            pauseOnVisibilityChange: true
+          }}
+          modules={[Autoplay]}>
+          <SwiperSlide><p>Fast Learner</p></SwiperSlide>
+          <SwiperSlide><p>Problem Solver</p></SwiperSlide>
+          <SwiperSlide><p>Team Player</p></SwiperSlide>
+        </Swiper>
+      </div>
+    </>
   );
 };
 
